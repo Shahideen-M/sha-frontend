5630f29c9a66006bd8a3cfb3a14ee877706ec0b2
Maligelli Shahideen <mshahideen64@gmail.com>
2026-07-24 17:18:13 +0530

    deploy: update production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//     baseURL: "http://localhost:8080",
+// });
+
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://sha-backend-y0r0.onrender.com",
 });
 
 export default api;
